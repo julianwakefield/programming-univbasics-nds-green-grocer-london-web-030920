bf2e19cb6be544c8b923e4274da1abd5c16af5fe
Julian Wakefield <julianwakefield@outlook.com>
2020-03-08 18:59:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -92,7 +92,7 @@ end
 
 
 def checkout(cart, coupons)
-  consolidated_cart = consolidated_cart(cart)
+  consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidate_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
 
