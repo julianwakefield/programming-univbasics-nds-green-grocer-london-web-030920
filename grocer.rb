8d30012e94a3f0634f779e6e5c582f909bdbf896
Julian Wakefield <julianwakefield@outlook.com>
2020-03-08 18:29:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,7 +53,15 @@ def apply_coupons(cart, coupons)
         if cart_item_with_coupon
           cart_item_with_coupon[:count] += coupons[counter][:num]
           cart_item[:count] -= coupons[counter][:num]
-
+        else
+          cart_item_with_coupon = {
+            :item => couponed_item_name,
+            :price => coupons[counter][:cost] / coupons[counter][:num],
+            :count => coupons[counter][:num],
+            :clearance => cart_item[:clearance]
+          }
+                cart << cart_item_with_coupon
+                cart_item[:count] -= coupons[counter][:num]
             end
         end
 
